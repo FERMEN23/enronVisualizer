a4a5adb04dde0fe62b55f8788ba6880903c90498
FERMEN23 <fernandamendoza2300@gmail.com>
2023-07-10 13:10:20 -0600

    version1

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/FERMEN23/backend-go/controllers"
+	"github.com/FERMEN23/backend-go/middleware"
+	"github.com/go-chi/chi"
+)
+
+func main() {
+
+	router := chi.NewRouter()
+	router.Use(middleware.CorsMiddleware)
+	router.Get("/elementsId/{fromValue}", controllers.GetElementsFrom)
+	router.Get("/elementsFilter/{email}/{last}", controllers.GetElementsIdAndFilter)
+	router.Get("/elementsById/{id}", controllers.GetElementsByID)
+
+	log.Println("Server started on :8000")
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
